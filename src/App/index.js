5e31f7c7f56030251240cd6b86d0ee50b2120f14
jczuprynski <JimCzuprynski@users.noreply.github.com>
2020-09-24 08:28:35 -0500

    Add files via upload

--- src/App/index.js
+++ src/App/index.js
@@ -1,60 +1,29 @@
 import React from 'react';
-import AppContext, { useAppReducer } from '../AppContext';
-import Homepage from '../Homepage';
-import Nav from '../Nav';
-import TownPage from '../TownPage';
-import { locationsByTown } from '../locations';
 import './style.css';
 
-function App() {
-  const [state, dispatch] = useAppReducer();
-
-  function getTownSearchQueryValue() {
-    const params = window.location.search.replace('?', '').split('&');
-    const match = params.find((param) => param.startsWith('town='));
-
-    if (!match) {
-      return null;
-    }
-
-    return decodeURIComponent(match).replace('town=', '');
-  }
-
-  React.useEffect(() => {
-    const id = getTownSearchQueryValue();
-
-    if (id && !state.town) {
-      const town = locationsByTown[id];
-
-      if (town) {
-        dispatch((copy) => ({ ...copy, town, query: town.id }));
-      }
-    }
-  }, [state.town, dispatch]);
-
-  React.useEffect(() => {
-    if (state.town) {
-      const matches = getTownSearchQueryValue() === state.town.id;
-
-      if (!matches) {
-        window.history.pushState({}, '', `/?town=${encodeURIComponent(state.town.id)}`);
-      }
-    }
-  }, [
-    state.town,
-  ]);
-
+export default function Footer() {
   return (
-    <AppContext.Provider value={state}>
-      <div className="app">
-        <Nav />
-        <main>
-          {state.town && state.town !== '404' && <TownPage />}
-          {!state.town && <Homepage />}
-        </main>
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="faq">
+          <h3>What is a secure ballot dropbox?</h3>
+          <p>
+          All across the Illinois 6th Congressional District, cities and towns are installing dropboxes where voters can return their completed ballots that they received in the mail. 
+          If you requested and received a mail-in ballot, returning your ballot via a dropbox in your community is a safe and secure way to make sure that your vote is counted. 
+          Please make sure to first complete your ballot (vote for Sean!) and put it in the signed yellow envelope and white return envelope.
+          </p>
+        </div>
+        <p className="footnote">
+        We'll be updating this directory frequently. Please use this tool to search for the location of the ballot drop boxes in your community. 
+        You can only drop off your ballot in a dropbox in your own community. Please be advised that you cannot drop off your ballot at a polling place, 
+        only a secure dropbox or your local election office.
+        </p>
+        <p className="footnote">If you have more questions about voting, visit <a href="https://castenforcongress.com/vote-by-mail-form/">castenforcongress.com/vote</a>. 
+        Voter helpdesk: vote@castenforcongress.com, 
+        Voter protection hotline: (224) 555-1212
+        </p>
       </div>
-    </AppContext.Provider>
+      <span className="disclaimer">paid for by casten for congress</span>
+    </footer>
   );
 }
-
-export default App;
